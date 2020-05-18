8ff2c8a86a99031c2ec00dc80c5c9324e4b5f0e8
Fabian Gonzalez <fabiangonz98@gmail.com>
2020-05-18 01:32:25 -0500

    Updates

---
+++ precache-manifest.86fab5bc9f000b006e67c4873728ebb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf585e5749a62e8f5e39903ac1b73f69",
+    "url": "/infocus7.github.io/index.html"
+  },
+  {
+    "revision": "7f544f860ddd997cc401",
+    "url": "/infocus7.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cf187f8f8858e62157fc",
+    "url": "/infocus7.github.io/static/js/2.2d8941fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infocus7.github.io/static/js/2.2d8941fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f544f860ddd997cc401",
+    "url": "/infocus7.github.io/static/js/main.4a06a94f.chunk.js"
+  },
+  {
+    "revision": "064e34d9f8186901e181",
+    "url": "/infocus7.github.io/static/js/runtime-main.d359f8c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/infocus7.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
